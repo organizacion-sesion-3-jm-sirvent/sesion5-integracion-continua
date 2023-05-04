aa24252ab30ea1a40b0520eed23f36aa5370515b
José María Sirvent <131195099+jmsi1@users.noreply.github.com>
2023-05-04 15:41:52 +0200

    implementar 'par'

--- ejercicios/operaciones.py
+++ ejercicios/operaciones.py
@@ -3,3 +3,9 @@ def suma(x,y):
 
     # El alumno deberá implementar correctamente el código de la función para pasar el test correspondiente
     return x + y
+
+# Declaración del método
+#
+# @param: IN num    Número sobre el que se va a comprobar si es par o impar
+def par(num):
+    return (num % 2) == 0
